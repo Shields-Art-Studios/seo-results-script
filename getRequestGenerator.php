07db118f5b4ed98d4593cb1436d700718407c93d
Jack Woods <jackrwoods@gmail.com>
2019-03-13 13:17:46 -0700

    Base64 encode URL

--- getRequestGenerator.php
+++ getRequestGenerator.php
@@ -4,10 +4,10 @@
 # @Email:  jackrwoods@gmail.com
 # @Filename: getRequestGenerator.php
 # @Last modified by:   Jack Woods
-# @Last modified time: 2019-03-04T09:17:07-08:00
+# @Last modified time: 2019-03-13T13:17:22-07:00
 
   # Perform a cross-domain GET request
-  $location = file_get_contents($_GET['url']);
+  $location = file_get_contents(base64_decode($_GET['url']));
   # Print the HTML
   print $_GET['callback']."('".str_replace("\n", "", str_replace("'", "\"", htmlspecialchars($location)))."')";
  ?>
