0768069abb63a5d65e27a29cab963b34bc2c79cc
Jack Woods <jackrwoods@gmail.com>
2019-03-18 11:37:17 -0700

    Re-add parseurl()

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
  * @Email:  jackrwoods@gmail.com
  * @Filename: script.js
  * @Last modified by:   Jack Woods
- * @Last modified time: 2019-03-18T11:35:13-07:00
+ * @Last modified time: 2019-03-18T11:37:08-07:00
  */
 
 // Define classes
@@ -71,6 +71,46 @@ class Category {
 
 
 // Helper Functions for Tests
+// Parses a url for the hostname
+function parseURL(url) {
+    parsed_url = {}
+
+    if ( url == null || url.length == 0 )
+        return parsed_url
+
+    protocol_i = url.indexOf('://')
+    parsed_url.protocol = url.substr(0,protocol_i)
+
+    remaining_url = url.substr(protocol_i + 3, url.length)
+    domain_i = remaining_url.indexOf('/')
+    domain_i = domain_i == -1 ? remaining_url.length - 1 : domain_i
+    parsed_url.domain = remaining_url.substr(0, domain_i)
+    parsed_url.path = domain_i == -1 || domain_i + 1 == remaining_url.length ? null : remaining_url.substr(domain_i + 1, remaining_url.length)
+
+    domain_parts = parsed_url.domain.split('.')
+    switch ( domain_parts.length ){
+        case 2:
+          parsed_url.subdomain = null
+          parsed_url.host = domain_parts[0]
+          parsed_url.tld = domain_parts[1]
+          break
+        case 3:
+          parsed_url.subdomain = domain_parts[0]
+          parsed_url.host = domain_parts[1]
+          parsed_url.tld = domain_parts[2]
+          break
+        case 4:
+          parsed_url.subdomain = domain_parts[0]
+          parsed_url.host = domain_parts[1]
+          parsed_url.tld = domain_parts[2] + '.' + domain_parts[3]
+          break
+    }
+
+    parsed_url.parent_domain = parsed_url.host + '.' + parsed_url.tld
+
+    return parsed_url
+}
+
 // Counts word frequency and returns a list
 function keywords(html) {
   // Remove script tags and other nonsense
