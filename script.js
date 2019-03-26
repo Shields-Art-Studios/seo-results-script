8bfcf679c081a2bc5e434163b61762f82b8732b3
Jack Woods <jackrwoods@gmail.com>
2019-03-26 15:55:24 -0700

    Fix variable names

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
  * @Email:  jackrwoods@gmail.com
  * @Filename: script.js
  * @Last modified by:   Jack Woods
- * @Last modified time: 2019-03-26T15:51:13-07:00
+ * @Last modified time: 2019-03-26T15:55:13-07:00
  */
 
  // Add microformat parser to page
@@ -195,14 +195,14 @@ function keywords(html) {
     },
     keywords: (page, callbackObj) => {
       // TODO: Filter common words, Google Snippet Generator
-      let keywords = keywords(page)
+      let keywordList = keywords(page)
       let badKeywords = [
         'the',
         'a',
         '1'
       ]
       let results = []
-      keywords.forEach(k => {
+      keywordList.forEach(k => {
         if (badKeywords.indexOf(k.key) === -1) {
           results.push(new TestResult(k.frequency, UNNESTED, k.key))
         }
