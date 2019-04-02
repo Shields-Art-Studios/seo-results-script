d67f1e35bd24a35f789259b6e7f81307da3066d1
Jack Woods <jackrwoods@gmail.com>
2019-04-02 07:12:40 -0700

    Update mail script

--- sendEmail.php
+++ sendEmail.php
@@ -4,7 +4,7 @@
 # @Email:  jackrwoods@gmail.com
 # @Filename: sendEmail.php
 # @Last modified by:   Jack Woods
-# @Last modified time: 2019-03-28T08:45:54-07:00
+# @Last modified time: 2019-04-02T07:12:30-07:00
 
 
 # Get email data from POST url encoded body
@@ -12,19 +12,21 @@
 $to = $_POST["email"];
 $subject = "SEO Report";
 $txt = $_POST["message"];
-$headers = "From: gabriel@shieldsarts.com\r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+$headers = array("From: gabriel@shieldsarts.com",
+    "Reply-To: gabriel@shieldsarts.com",
+    "X-Mailer: PHP/" . PHP_VERSION
+);
 mail($to,$subject,$txt,$headers);
 
 $to2 = "gabriel@shieldsarts.com";
 $subject2 = "New Lead - SEO Report";
 $txt2 = $_POST["message2"].$_POST["message"];
-$headers2 = "From: gabriel@shieldsarts.com\r\n";
-$headers2 .= "MIME-Version: 1.0\r\n";
-$headers2 .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+$headers2 = array("From: gabriel@shieldsarts.com",
+    "Reply-To: gabriel@shieldsarts.com",
+    "X-Mailer: PHP/" . PHP_VERSION
+);
 
 mail($to2,$subject2,$txt2,$headers2);
 
-print_r("Sent!");
+print_r("Sent! to ".$_POST["email"]);
 ?>
